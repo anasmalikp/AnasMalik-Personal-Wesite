60d290cca2166923ff802d49375ff01d33101287
anasmalikp <anasmalikp@gmail.com>
2024-02-06 20:40:41 +0530

    portfolio form validation processed

--- portfolio.js
+++ portfolio.js
@@ -5,24 +5,3 @@ function showsidebar() {
 function hidesidebar() {
     const x = document.getElementById('sidebar').style.display='none';
 }
-function sendMail() {
-    var params= {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        message: document.getElementById('message').value,
-    }
-    const serviceID = "service_glmwcbe";
-const templateID = "template_44yf213";
-
-emailjs.send(serviceID,templateID,params)
-.then(
-    res=>{
-        document.getElementById('name').value = "";
-        document.getElementById('email').value = "";
-        document.getElementById('message').value = "";
-        console.log(res);
-        alert("Your Message Sent Successfully")
-    }
-)
-.catch(err=> console.log(err));
-}
